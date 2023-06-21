e4a8e7fec8751d519fa8067e18e5e0a003c31a90
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-21 16:21:12 +0800

    update

--- android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
+++ android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
@@ -1,8 +1,6 @@
 package com.nextgen.plugin;
 
 import android.util.Log;
-import android.content.Intent;
-import android.content.Context;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
@@ -18,8 +16,8 @@ import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 public class NextgenAppPluginPlugin extends Plugin {
 
     private IWXAPI api;
+    private WXEntryActivity activity = new WXEntryActivity();
     private NextgenAppPlugin implementation = new NextgenAppPlugin();
-    private String CallbackId = "";
 
     @PluginMethod
     public void echo(PluginCall call) {
@@ -32,46 +30,19 @@ public class NextgenAppPluginPlugin extends Plugin {
     @PluginMethod
     public void wxInit(PluginCall call) {
         Log.i("Echo", "Wx Init");
-
-        IWXAPIEventHandler wxEventHandler = new IWXAPIEventHandler() {
-            public void onResp(BaseResp baseResp) {
-                // 在這裡處理微信回應事件
-                if (baseResp == SendTdiAuth.Resp) {
-                    SendAuth.Resp authResp = (SendTdiAuth.Resp) baseResp;
-                    String code = authResp.code;
-                    PluginCall savedCall = bridge.getSavedCall(CallbackId);
-                    if (savedCall != null) {
-                        if (code != null) {
-                            JSObject ret = new JSObject();
-                            // 在這裡處理登錄成功後的邏輯，例如獲取用戶資訊等
-                            ret.put("token", code);
-                            savedCall.resolve(ret);
-                        } else {
-                            // 登錄失敗
-                            savedCall.reject("LOGIN_FAILED");
-                        }
-                        bridge.releaseCall(CallbackId);
-                    }
-                }
-            };
-        };
-
-        Context context = bridge.getContext();
         String appId = call.getString("appId");
-        api = WXAPIFactory.createWXAPI(context, appId, true);
+        api = WXAPIFactory.createWXAPI(activity, appId, true);
         api.registerApp(appId);
-        api.handleIntent(getIntent(), wxEventHandler);
         call.resolve();
     }
 
     @PluginMethod
     public void wxLogin(PluginCall call) {
         Log.i("Echo", "Wx Login");
+        activity.regCallback(call);
         final SendAuth.Req req = new SendAuth.Req();
         req.scope = "snsapi_userinfo";
         req.state = "wechat";
         api.sendReq(req);
-        CallbackId = call.getCallbackId();
-        bridge.saveCall(call);
     }
 }
