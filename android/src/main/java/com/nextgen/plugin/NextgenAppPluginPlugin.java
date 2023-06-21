1d8b0a6cd0f2844d7f6c12b533c875f66927af91
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-21 16:33:36 +0800

    update

--- android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
+++ android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
@@ -12,6 +12,46 @@ import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
 
+
+
+import android.app.Activity;
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+
+class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+
+    private PluginCall callback;
+
+    public void regCallback(PluginCall cb) {
+        callback = cb;
+    }
+
+    @Override
+    public void onReq(BaseReq baseReq) {}
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        // 在這裡處理微信回應事件
+        if (baseResp instanceof SendAuth.Resp) {
+            SendAuth.Resp authResp = (SendAuth.Resp) baseResp;
+            String code = authResp.code;
+            if (callback != null) {
+                if (code != null) {
+                    JSObject ret = new JSObject();
+                    // 在這裡處理登錄成功後的邏輯，例如獲取用戶資訊等
+                    ret.put("token", code);
+                    callback.resolve(ret);
+                } else {
+                    // 登錄失敗
+                    callback.reject("LOGIN_FAILED");
+                }
+                callback = null;
+            }
+        }
+    }
+}
+
 @CapacitorPlugin(name = "NextgenAppPlugin")
 public class NextgenAppPluginPlugin extends Plugin {
 
