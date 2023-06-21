e553a19bd357337208254994e2d7fdf519bca0a2
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-21 17:55:24 +0800

    update

--- android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
+++ android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
@@ -1,8 +1,6 @@
 package com.nextgen.plugin;
 
-import android.os.Bundle;
 import android.util.Log;
-import android.app.Activity;
 
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
@@ -11,36 +9,33 @@ import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
-import com.tencent.mm.opensdk.modelbase.BaseReq;
 import com.tencent.mm.opensdk.modelbase.BaseResp;
 
 import com.tencent.mm.opensdk.openapi.IWXAPI;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 
-class WXEntryActivity extends Activity implements IWXAPIEventHandler {
-    private String appId;
+@CapacitorPlugin(name = "NextgenAppPlugin")
+public class NextgenAppPluginPlugin extends Plugin {
     private IWXAPI api;
     private PluginCall callback;
-    public WXEntryActivity(String aid) {
-        appId = aid;
+    private NextgenAppPlugin implementation = new NextgenAppPlugin();
+
+    @PluginMethod
+    public void echo(PluginCall call) {
+        String value = call.getString("value");
+        JSObject ret = new JSObject();
+        ret.put("value", implementation.echo(value));
+        call.resolve(ret);
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState){
-        super.onCreate(savedInstanceState);
-        api = WXAPIFactory.createWXAPI(this, appId, true);
+    @PluginMethod
+    public void wxInit(PluginCall call) {
+        Log.i("Echo", "Wx Init");
+        String appId = call.getString("appId");
+        api = WXAPIFactory.createWXAPI(this.getContext(), appId, true);
         api.registerApp(appId);
-        api.handleIntent(getIntent(), this);
-        Log.i("Echo", "Wx Inited");
+        call.resolve();
     }
-
-    @Override
-    public void onReq(BaseReq baseReq) {
-        finish();
-    }
-
-    @Override
     public void onResp(BaseResp baseResp) {
         // 在這裡處理微信回應事件
         if (baseResp instanceof SendAuth.Resp) {
@@ -60,46 +55,13 @@ class WXEntryActivity extends Activity implements IWXAPIEventHandler {
             }
         }
     }
-
-    public void login(PluginCall call) {
-        Log.i("Echo", "Wx Login");
-        Log.i("Echo", api != null ? "A" : "N");
-        if (api != null && api.isWXAppInstalled()) {
-            final SendAuth.Req req = new SendAuth.Req();
-            req.scope = "snsapi_userinfo";
-            req.state = "wechat";
-            callback = call;
-            api.sendReq(req);
-        } else {
-            Log.i("Echo", "Wx no Installed");
-        }
-    }
-}
-
-@CapacitorPlugin(name = "NextgenAppPlugin")
-public class NextgenAppPluginPlugin extends Plugin {
-    private WXEntryActivity activity;
-    private NextgenAppPlugin implementation = new NextgenAppPlugin();
-
-    @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
-    }
-
-    @PluginMethod
-    public void wxInit(PluginCall call) {
-        Log.i("Echo", "Wx Init");
-        String appId = call.getString("appId");
-        activity = new WXEntryActivity(appId);
-        call.resolve();
-    }
-
     @PluginMethod
     public void wxLogin(PluginCall call) {
         Log.i("Echo", "Wx Login");
-        activity.login(call);
+        final SendAuth.Req req = new SendAuth.Req();
+        req.scope = "snsapi_userinfo";
+        req.state = "wechat";
+        callback = call;
+        api.sendReq(req);
     }
 }
