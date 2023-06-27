9223a8abba29b701ed4b1e5ba10a40ab153f88a2
nextgen <nextgen@khczhihaodeMacBook-Air.local>
2023-06-27 12:08:56 +0800

    update

--- android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
+++ android/src/main/java/com/nextgen/plugin/NextgenAppPluginPlugin.java
@@ -8,47 +8,12 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-import com.tencent.mm.opensdk.modelbase.BaseReq;
-import com.tencent.mm.opensdk.modelbase.BaseResp;
 import com.tencent.mm.opensdk.modelmsg.SendAuth;
 import com.tencent.mm.opensdk.openapi.IWXAPI;
-import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
 import com.tencent.mm.opensdk.openapi.WXAPIFactory;
-
-class Handler implements IWXAPIEventHandler {
-    PluginCall callback;
-    public void onReq(BaseReq baseReq) {}
-    public void onResp(BaseResp baseResp) {
-        // 在這裡處理微信回應事件
-        Log.i("Echo", "Wx Callback");
-        Log.i("Echo", baseResp.toString());
-        if (baseResp instanceof SendAuth.Resp) {
-            SendAuth.Resp authResp = (SendAuth.Resp) baseResp;
-            String code = authResp.code;
-            if (callback != null) {
-                if (code != null) {
-                    JSObject ret = new JSObject();
-                    // 在這裡處理登錄成功後的邏輯，例如獲取用戶資訊等
-                    ret.put("token", code);
-                    callback.resolve(ret);
-                } else {
-                    // 登錄失敗
-                    callback.reject("LOGIN_FAILED");
-                }
-                callback = null;
-            }
-        }
-    }
-    public void onSendFinished() {
-        Log.i("Echo", "Wx Finished");
-    }
-}
 
 @CapacitorPlugin(name = "NextgenAppPlugin")
 public class NextgenAppPluginPlugin extends Plugin {
-    private IWXAPI api;
-    private Handler handler = new Handler();
-
     private NextgenAppPlugin implementation = new NextgenAppPlugin();
 
     @PluginMethod
@@ -63,9 +28,8 @@ public class NextgenAppPluginPlugin extends Plugin {
     public void wxInit(PluginCall call) {
         Log.i("Echo", "Wx Init");
         String appId = call.getString("appId");
-        api = WXAPIFactory.createWXAPI(getContext(), appId, false);
-        api.registerApp(appId);
-        api.handleIntent(getActivity().getIntent(), handler);
+        WXEntryActivity.api = WXAPIFactory.createWXAPI(getContext(), appId, false);
+        WXEntryActivity.api.registerApp(appId);
         call.resolve();
     }
 
@@ -73,9 +37,8 @@ public class NextgenAppPluginPlugin extends Plugin {
     public void wxLogin(PluginCall call) {
         Log.i("Echo", "Wx Login");
         final SendAuth.Req req = new SendAuth.Req();
-        handler.callback = call;
         req.scope = "snsapi_userinfo";
         req.state = "wechat";
-        api.sendReq(req);
+        WXEntryActivity.api.sendReq(req);
     }
 }
---
+++ android/src/main/java/com/nextgen/plugin/WXEntryActivity.java
@@ -0,0 +1,69 @@
+package com.nextgen.plugin;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import com.getcapacitor.JSObject;
+import com.getcapacitor.PluginCall;
+
+import com.tencent.mm.opensdk.modelbase.BaseReq;
+import com.tencent.mm.opensdk.modelbase.BaseResp;
+import com.tencent.mm.opensdk.modelmsg.SendAuth;
+import com.tencent.mm.opensdk.openapi.IWXAPI;
+import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.opensdk.openapi.WXAPIFactory;
+
+
+public class WXEntryActivity extends Activity implements IWXAPIEventHandler {
+    static IWXAPI api;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("Echo", "Wx On Create");
+        var APP_ID = "1234";
+        api = WXAPIFactory.createWXAPI(this, APP_ID, false);
+        try {
+            Intent intent = getIntent();
+            api.handleIntent(intent, this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        api.handleIntent(intent, this);
+    }
+
+    @Override
+    public void onReq(BaseReq req) {
+        finish();
+    }
+
+    @Override
+    public void onResp(BaseResp baseResp) {
+        Log.i("Echo", "Wx Callback");
+        Log.i("Echo", baseResp.toString());
+        if (baseResp instanceof SendAuth.Resp) {
+//            SendAuth.Resp authResp = (SendAuth.Resp) baseResp;
+//            String code = authResp.code;
+//            if (callback != null) {
+//                if (code != null) {
+//                    JSObject ret = new JSObject();
+//                    // 在這裡處理登錄成功後的邏輯，例如獲取用戶資訊等
+//                    ret.put("token", code);
+//                    callback.resolve(ret);
+//                } else {
+//                    // 登錄失敗
+//                    callback.reject("LOGIN_FAILED");
+//                }
+//                callback = null;
+//            }
+        }
+        finish();
+    }
+}
