1a4ea5ffba031d77f349ea5f5174186847541403
zhihao <zhihaozheng@nextgen.com.hk>
2023-08-23 19:48:50 +0800

    update

--- ios/Plugin/NextgenAppPluginPlugin.h
+++ ios/Plugin/NextgenAppPluginPlugin.h
@@ -1,5 +1,5 @@
 #import <UIKit/UIKit.h>
-
+#import <WXApi.h>
 //! Project version number for Plugin.
 FOUNDATION_EXPORT double PluginVersionNumber;
 
@@ -8,3 +8,8 @@ FOUNDATION_EXPORT const unsigned char PluginVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Plugin/PublicHeader.h>
 
+@interface AppDelegate : UIResponder<UIApplicationDelegate, WXApiDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- ios/Plugin/NextgenAppPluginPlugin.swift
+++ ios/Plugin/NextgenAppPluginPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+
 // TODO:
 // import WXApi.h
 
